08dac41e8c4c25c79053cb6dd0b2584374f09362
Мар'яна Легещич <vladyslav.filimonchuk@gmail.com>
2023-04-27 16:38:38 +0300

    3rd commit

--- src/scripts/buttonScripts.js
+++ src/scripts/buttonScripts.js
@@ -29,31 +29,3 @@ function reset(){
 function pickColor(color){
 	document.querySelector(".currentColor").style.background = color.style.background;
 }
-
-
-
-
-
-
-
-
-function PrintDiv(div)
-{
-    html2canvas((div), {
-        onrendered: function(canvas) {
-            var myImage = canvas.toDataURL();
-            downloadURI(myImage, "MaSimulation.png");
-      }
-    });
-}
-
-function downloadURI(uri, name) {
-    var link = document.createElement("a");
-
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();   
-    //after creating link you should delete dynamic link
-    //clearDynamicLink(link); 
-}
