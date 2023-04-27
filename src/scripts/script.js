a9d2dc70096571f4f44d128c82bcdeb4def0263b
Мар'яна Легещич <vladyslav.filimonchuk@gmail.com>
2023-04-27 12:47:03 +0300

    2nd commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,15 @@
 window.onload = () => {
-	tableCreator();
+	tableCreator(8);
 }
 
-function tableCreator() {
+function tableCreator(tableSize) {
 
 	let table = document.createElement('table');
 	table.classList.add("tableToDraw");
+	table.id = "tableToDraw";
+
 	document.getElementById('pixelArtDrawWorkspace').appendChild(table);
 
-	let tableSize = 32;
 	for (let i = 0; i < tableSize; i++) {
 		let tr = document.createElement('tr');
 		tr.id = "trId" + i;
@@ -17,17 +18,43 @@ function tableCreator() {
 		for (let k = 0; k < tableSize; k++) {
 			let th = document.createElement('th');
 			th.id = "thId" + k;
-			th.addEventListener('click', this.onclick)
+			th.addEventListener('click', function (event) {
+  				draw(this);
+  			})
 			tr.appendChild(th);
 		}
 	}
 }
 
-
-
 function draw(cell) {
-	console.log(cell);
-	this.onclick = function(event) {
-        console.log(this); 
-      };
+
+	cell.style.background = "red";
+}
+
+function resize(){
+
+	let pixelArtDrawWorkspace = document.getElementById('pixelArtDrawWorkspace');
+	let inputSize = document.getElementById('resizeInputField');
+
+	if (Number(inputSize.value) >= 2 && Number(inputSize.value) <= 64) {
+		pixelArtDrawWorkspace.removeChild(pixelArtDrawWorkspace.children[0]);
+		tableCreator(inputSize.value);
+
+		inputSize.classList.replace('resizeErrorInputField', 'resizeInputField');
+	}
+	else {
+		inputSize.classList.replace('resizeInputField', 'resizeErrorInputField');
+	}
+}
+
+function reset(){
+
+	let pixelArtDrawWorkspace = document.getElementById('pixelArtDrawWorkspace');
+	let inputSize = document.getElementById('resizeInputField');
+	let tableSize = Number(document.getElementById('tableToDraw').childElementCount);
+
+	inputSize.classList.replace('resizeErrorInputField', 'resizeInputField');
+	
+	pixelArtDrawWorkspace.removeChild(pixelArtDrawWorkspace.children[0]);
+	tableCreator(tableSize);
 }
